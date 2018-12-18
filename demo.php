454c8c871b0443d117779093b45b294e9436200d
yangxuguang <yangxuguangvip@163.com>
2018-12-18 17:57:26 +0800

    test

--- demo.php
+++ demo.php
@@ -1,61 +1,42 @@
 <?php
 
-require_once dirname(__FILE__).'/vendor/autoload.php';
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-use YQ\YqExtend;
-use YQ\Elastic\DemoElastic;
-use YQ\Caches\YqWeixinJsapiTicketCache;
-use YQ\YqEmail;
+// use YQ\YqExcel;
+ use YQ\YqExtend;
 
-// $client = DemoElastic::getInstance();
+// $ret = YqExcel::read('./payssion.xlsx', 'Sheet1');
+// $ret = $ret[0];
+// for ($i = 1; $i < count($ret); $i++) {
+//     $tmp = [];
+//     foreach ($ret[$i] as $key => $val) {
+//         $k       = $ret[0][$key];
+//         $tmp[$k] = $val;
+//     }
+//     $data[] = $tmp;
+// }
 
-// $ret003 = $client->mapping();
+// $payssions = [];
+// foreach ($data as $val) {
+//     $country_name = $val['country_name'];
+//     if (!isset($payssions[$country_name])) {
+//         $payssions[$country_name] = [
+//             'country_name' => $country_name,
+//             'country'      => $val['country'],
+//             'country_icon' => $val['country_icon'],
+//             'pays'         => [],
+//         ];
+//     }
+//     $payssions[$country_name]['pays'][] = [
+//         'pay_type'   => $val['pay_type'],
+//         'pm_id'      => $val['pm_id'],
+//         'need_email' => $val['need_email'],
+//     ];
+// }
 
-// $ret004 = $client->setDocument(1002, [
-//     'booth_id' => 1002,
-//     'create_time' => time(),
-//     'name' => '连州糖水',
-//     'trade_id' => 1,
-//     'summary' => '好喝下火，香甜可口，过来看看瞧瞧吧',
-// ]);
+// echo var_export($payssions, true);
+// echo json_encode($payssions, JSON_UNESCAPED_UNICODE);
 
-// $ret005 = $client->getDocument(1002);
+// echo date('G', time());
 
-// $ret006 = $client->updateDocument(1002, [
-//     'summary' => '都是骗人的',
-// ]);
-
-// print_r($ret005);
-
-
-
-// YqWeixinJsapiTicketCache::getInstance()->update(1001, [
-//     'booth_id' => 1002,
-//     'create_time' => time(),
-//     'name' => '连州糖水',
-//     'trade_id' => 1,
-//     'summary' => '好喝下火，香甜可口，过来看看瞧瞧吧111111111111111111',
-// ]);
-
-
-// $ret007 = YqWeixinJsapiTicketCache::getInstance()->get(1001);
-
-// print_r($ret007);
-
-
-
-// 存放文件目录
-// $path = $putname = YqExtend::uniqid();
-// print_r($path);
-// print_r($putname);
-
-
-
-// $params = "%7B%22userid%22%3A1118277%2C%22body%22%3A%221980%5Cu91d1%5Cu5e01%22%2C%22fee%22%3A%22198%22%2C%22subject%22%3A%221980%5Cu91d1%5Cu5e01%22%2C%22appId%22%3A%22649%22%2C%22trade_sn%22%3A%22i2018020918465010824%22%2C%22orderId%22%3A%22dba05bedb0157554b4fd222f5e5bc96f%22%2C%22status%22%3A%22succ%22%2C%22createTime%22%3A%222018-02-09_18%3A46%3A50%22%2C%22sign%22%3A%22c41d996f26e4f6c98537abcad993962c%22%7D=";
-// print_r(urldecode($params));
-// $params = json_decode(urldecode($params), true);
-// print_r($params);
-
-
-// YqEmail::test();
-YqEmail::test2();
+echo YqExtend::getRandom(9);
