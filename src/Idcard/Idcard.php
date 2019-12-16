c3a35b40feb044342ce22ed5929efd554f8687ab
yangxgkem <yangxuguangvip@163.com>
2019-12-16 10:12:12 +0800

    修复身份证年龄判断

--- src/Idcard/Idcard.php
+++ src/Idcard/Idcard.php
@@ -149,10 +149,10 @@ class Idcard
         $birthday = $this->getBirthday("-");
         if ($birthday) {
             list($year, $month, $day) = explode("-", $birthday);
-            $year_diff  = date("Y") - $year;
-            $month_diff = date("m") - $month;
-            $day_diff   = date("d") - $day;
-            if ($day_diff < 0 || $month_diff < 0) {
+            $year_diff                = date("Y") - $year;
+            $month_diff               = date("m") - $month;
+            $day_diff                 = date("d") - $day;
+            if ($month_diff < 0 || ($month_diff == 0 && $day_diff < 0)) {
                 $year_diff--; // 不满一岁
             }
             return intval($year_diff);
